d0ca84a11ec430869d1fe1810c09359f1baacea7
Diya-coder2014 <stu13311@dpsmisdoha.com>
2026-07-16 16:56:52 +0530

    Activity 1 and 2 completed!

---
+++ Activity 1.py
@@ -0,0 +1,14 @@
+
+print("Data types in python")
+
+a = 2
+print("The type of a:", type(a))
+
+b = 2.32
+print("The type of b:", type(b))
+
+c="codingal"
+print("The type of c:", type(c))
+
+d = True
+print("The type of d:", type(d))
---
+++ Activity 2 .py
@@ -0,0 +1,24 @@
+print("TypeCasting")
+
+name = "penguin"
+age = 12
+weight = 38.5
+is_student = True
+
+print(name)
+print(age)
+print(weight)
+print(is_student)
+
+print("Data type of 'name':", type(name))
+print("Data type of 'age':", type(age))
+print("Data type of 'weight':", type(weight))
+print("Data type of 'is_student':", type(is_student))
+
+new_age = str(age)
+print(new_age)
+print("Data type of 'new_age':",new_age)
+
+new_is_student = int(weight)
+print(new_is_student)
+print("Data type of 'new_age':",new_is_student)
